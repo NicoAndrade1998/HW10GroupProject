028aabbef2b6d7724df045a5c17cdff670bc0656
Nico <Nico@DESKTOP-69JMNOJ>
2023-04-18 16:49:18 -0700

    Add project files.

---
+++ HW10GroupProject/Program.fs
@@ -0,0 +1,10 @@
+﻿(* HW10 - F# Task Manager with Queue
+ * Nico Andrade
+ * Edgar Fong
+ * Saul Romero
+ *)
+open System
+
+let create_job_list jobCount resultLis =
+    let rnd = Random()
+    List.init jobCount (fun _ -> rnd.Next(1, 100))
