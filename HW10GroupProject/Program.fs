17da4a51042a17fa1740587866485f37b576af13
Nicolas Andrade <nicoandrade1998@gmail.com>
2023-04-19 22:36:22 -0700

    removed unneccesary comments, rewrote code to be more agnostic toward size of list

--- HW10GroupProject/Program.fs
+++ HW10GroupProject/Program.fs
@@ -32,34 +32,23 @@ type Queue<'a> =
             q.front <- tl
             Some hd
 
-let q = { front = []; back = [] }
-//q.enqueue 23
-//q.enqueue 1
-//q.enqueue 7
-//printfn "%A" (q.dequeue())
-//printfn "%A" (q.dequeue())
-//printfn "%A" (q.dequeue())
-//printfn "%d" (Option.defaultValue 0 (q.dequeue()))
-//printfn "%d" (Option.defaultValue 0 (q.dequeue()))
-//printfn "%d" (Option.defaultValue 0 (q.dequeue()))
-
-
-
 
 (* 1. Create a List of random Numbers
  * 2. Add these numbers to a Queue
  * 3. <third thing here>
  *)
 let main =
-    //let lis = create_job_list 10 0
-    let lis = [3; 4; 6; 1; 9; 2; 5; 6;]
+    let length = 10
+    let q = { front = []; back = [] }
+    let lis = create_job_list length 0
+    //let lis = [3; 4; 6; 1; 9; 2; 5; 6;]   //used for testing
     printfn "Generated List: %A" lis
-    for i in 0 .. 7 do
+    for i in 0 .. (length - 1) do
         if lis[i] % 2 = 1 then
             q.enqueue lis[i]
             printf "Added task No. %d into the job queue" lis[i]
             printf "   Remaining List:" 
-            for x in (i+1) .. 7 do
+            for x in (i+1) .. (length - 1) do
                 printf "%d " lis[x]
             printf "\n"
         else
@@ -68,51 +57,12 @@ let main =
             | Some job ->
                 printf "Do job %A now" job
                 printf "   Remaining List:" 
-                for x in (i+1) .. 7 do
+                for x in (i+1) .. (length - 1) do
                     printf "%d " lis[x]
                 printf "\n"
             | None ->
                 printf "no more jobs to do right now"
                 printf "\n"
             
-    //for i in 1 .. 10 do
-        //printfn "%A" (q.dequeue())
+
    
-    
-
-
-
-
-
-
-
-
-//Test commit. Saul
-
-// compare_lists is not currently being used, I have attached it for reference purposes -Nico
-(* Compares two inputted lists to see if they are equal
- * If both lists are empty, evaluates true
- * If one list is longer than the other, evaluates true
- * If both lists have an element for the index, check if they are equal and if each subsequent eval is true. *)
-let rec compare_lists l1 l2 =
-    match l1, l2 with
-    | [] , [] -> true
-    | [] , _  -> false
-    | _ , []  -> false
-    | x::xs, y::ys -> x = y && compare_lists xs ys
-
-
-
-
-
-(* //Alternate version of jobcount generator
-let create_job_list2 jobCount resultLis =
-    let rec create_job_list1 jobCount resultLis =
-        match resultLis with
-        | [] -> [System.Random().Next()]
-        | head :: tail' -> head :: create_job_list1 jobCount tail'
-    create_job_list1 resultLis
-
-let newlis = create_job_list2 10 0
-printfn "%A" newlis
-*)
